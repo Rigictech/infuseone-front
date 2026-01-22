1788d85023e682bdba81e6853b42fe9af45be0a4
SmitRigic <tech@rigicglobalsolutions.com>
2026-01-22 17:51:19 +0530

    Update UserModal.jsx

--- src/components/UserModal.jsx
+++ src/components/UserModal.jsx
@@ -32,7 +32,7 @@ const UserModal = ({ show, onHide, onSubmit, user, title }) => {
     };
 
     const generatePassword = () => {
-        const length = 12;
+        const length = 8;
         const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
         let newPassword = "";
         for (let i = 0, n = charset.length; i < length; ++i) {
